216ec160bf8c99a061a1657bc45422ca8da9b3a3
Davidyoon <davidyoon@koreainvestment.com>
2023-04-17 14:19:58 +0900

    Add an AppInfoResponseModel
    * Added an AppInfoResponseModel
    * Set the directory structure

---
+++ DispatchQueueSample/AppInfoResponseModel.swift
@@ -0,0 +1,60 @@
+//
+//  AppInfoResponseModel.swift
+//  DispatchQueueSample
+//
+//  Created by jiwon Yoon on 2023/04/17.
+//
+
+import Foundation
+
+struct AppInfoResponseModel: Decodable {
+    let resultCount: Int
+    let results: [AppInfoModel]
+}
+
+struct AppInfoModel: Decodable {
+    let isGameCenterEnabled: Bool
+    let supportedDevices: [String]
+    let advisories: [String]
+    let features: [String]
+    let appletvScreenshotUrls: [String]
+    let screenshotUrls: [String]
+    let ipadScreenshotUrls: [String]
+    let artworkUrl512: String
+    let kind: String
+    let artworkUrl60: String
+    let artworkUrl100: String
+    let artistViewUrl: String
+    let trackCensoredName: String
+    let trackViewUrl: String
+    let releaseNotes: String
+    let artistId: Int
+    let artistName: String
+    let genres: [String]
+    let price: Double
+    let description: String
+    let bundleId: String
+    let sellerName: String
+    let primaryGenreName: String
+    let primaryGenreId: Int
+    let trackId: Int
+    let trackName: String
+    let genreIds: [String]
+    let releaseDate: String
+    let isVppDeviceBasedLicensingEnabled: Bool
+    let currentVersionReleaseDate: String
+    let averageUserRatingForCurrentVersion: Double
+    let userRatingCountForCurrentVersion: Int
+    let fileSizeBytes: String
+    let sellerUrl: String
+    let formattedPrice: String
+    let trackContentRating: String
+    let languageCodesISO2A: [String]
+    let minimumOsVersion: String
+    let version: String
+    let wrapperType: String
+    let contentAdvisoryRating: String
+    let averageUserRating: Double
+    let currency: String
+    let userRatingCount: Int
+}
---
+++ DispatchQueueSample/Service/AppStoreService.swift
@@ -0,0 +1,36 @@
+//
+//  AppStoreService.swift
+//  DispatchQueueSample
+//
+//  Created by jiwon Yoon on 2023/04/17.
+//
+
+import Foundation
+
+protocol service {
+    func requestService()
+}
+
+
+final class AppStoreService: service {
+    
+    func requestService() {
+        
+        let urlRequest = URLRequest(url: URL(string: "https://itunes.apple.com/lookup?bundleId=com.truefriend.neosmartirenewal")!)
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            guard error == nil else {
+                print("error :\(error?.localizedDescription)")
+                return
+            }
+            
+            if let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 {
+                do {
+                    
+                } catch(let error) {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+        .resume()
+    }
+}
